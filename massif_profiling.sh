2401a6f71604d898283de4163f24bfd037d171be
dannerju <julian.danner@uni-passau.de>
2023-05-16 16:49:04 +0200

    update profiling scripts

--- massif_profiling.sh
+++ massif_profiling.sh
@@ -3,9 +3,7 @@
 echo "profiling xnf_solver with valgrind-massif"
 
 #running valgrind tools
-#valgrind --tool=massif --time-unit=B --stacks=yes build/2xnf_solver ../benchmarks/instances/2xnfs/rand/rand-10-20.xnf
-valgrind --tool=massif --time-unit=B --stacks=yes build/2xnf_solver ../benchmarks/instances/2xnfs/bivium/bivium-t200-g40.xnf -t 30
-#valgrind --tool=massif --stacks=yes build/2xnf_solver ../benchmarks/instances/2xnfs/rand/rand-20-60.xnf
+valgrind --tool=massif --time-unit=B --stacks=yes build/xnf_cdcl_solver ../benchmarks/instances/2xnfs/matrix_multiplication/MM-22.xnf -ca no -t 60 -dh lwl
 
 #ms_print massif.out.*
 massif-visualizer massif.out.*
--- valgrind_profiling.sh
+++ valgrind_profiling.sh
@@ -3,7 +3,9 @@
 echo "profiling xnf_solver"
 
 #running valgrind tools
-valgrind --tool=callgrind --dump-instr=yes build/xnf_cdcl_solver ../benchmarks/instances/2xnfs/rand/rand-20-60.xnf
+#valgrind --tool=callgrind --dump-instr=yes build/xnf_cdcl_solver ../benchmarks/instances/2xnfs/rand/rand-20-60.xnf
+#valgrind --tool=callgrind --dump-instr=yes build/xnf_cdcl_solver ../benchmarks/instances/2xnfs/rand/rand-30-90.xnf -ca no -t 60 -dh lwl
+valgrind --tool=callgrind --dump-instr=yes build/xnf_cdcl_solver ../benchmarks/instances/2xnfs/matrix_multiplication/MM-22.xnf -ca no -t 60 -dh lwl
 #valgrind --tool=callgrind build/xnf_cdcl_solver ../benchmarks/instances/2xnfs/bivium/bivium-t150-g40.xnf -t 30
 #valgrind --tool=cachegrind build/xnf_cdcl_solver ../benchmarks/instances/2xnfs/rand/rand-20-60.xnf
 
