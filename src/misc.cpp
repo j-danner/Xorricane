e70d7b129ec9c1315db747ec293b8f33492860d7
Julian Danner <julian@danner-web.de>
2024-08-14 15:42:59 +0200

    print sols only once with --maxsol

--- src/misc.cpp
+++ src/misc.cpp
@@ -55,8 +55,8 @@ int solve(const vec< vec<xlit> >& xnf, const var_t num_vars, const options& opts
     s.end = std::chrono::high_resolution_clock::now();
     if(opts.verb>0) s.print_final();
 
-    s.print_sol();
     if(opts.sol_count>1) std::cout << "c solutions found: "+std::to_string( std::count_if(s.sols.begin(),s.sols.end(), [](const vec<bool>& sol){ return sol.size()>0; } ) ) << std::endl;
+    else s.print_sol();
 
     if(opts.verb >= 120) { std::cout << opts.to_str() << std::endl; }
     
