9c9aca03bfc302a0943ae69ab701171495ee5907
dannerju <julian.danner@uni-passau.de>
2025-01-19 21:35:44 +0100

    Revert "change IG preprocessing default to scc"
    This reverts commit ce0f953af9c289e5cd8ad864189f994c0e7c6eb8.

--- src/main.cpp
+++ src/main.cpp
@@ -131,7 +131,7 @@ int main(int argc, char const *argv[])
     //preproc (SCC+FLS)
     program.add_argument("-pp","--preprocess")
         .help("proprocessing via implication graphs (see 2-Xornado); 'no' (no), 'scc' (strongly connected components), or 'scc_fls' (strongly connected components and failed linerals)")
-        .default_value(std::string("scc"))
+        .default_value(std::string("scc_fls"))
         .choices("no", "scc", "scc_fls")
         .nargs(1);
     
--- src/misc.hpp
+++ src/misc.hpp
@@ -249,7 +249,7 @@ struct options {
     restart_opt rst = restart_opt::luby;
 
     initial_prop_opt ip = initial_prop_opt::no;
-    xornado_preproc pp = xornado_preproc::scc;
+    xornado_preproc pp = xornado_preproc::scc_fls;
 
     bool eq = true;
 
